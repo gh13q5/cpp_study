c49b735ab00dd9adb3f5bc18ec1326c964f18b21
g.h <35606917+gh13q5@users.noreply.github.com>
2023-06-21 16:16:27 +0900

    Create 짝지어 제거하기.cpp

---
+++ 프로그래머스/Lv2/짝지어 제거하기.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <stack>
+
+using namespace std;
+
+int solution(string s){
+    stack<int> st;
+    st.push(s[0]);
+    for(int i = 1; i < s.length(); i++){
+        if(!st.empty() && st.top() == s[i])   st.pop();
+        else    st.push(s[i]);
+    }
+    
+    if(st.empty())
+        return 1;
+    return 0;
+}
